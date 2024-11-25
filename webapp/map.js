d07ada0110de0a1f1ea22de72e08b5094b9c9fc1
mixalis20 <mtsatsakoulias@gmail.com>
2024-11-25 15:34:37 +0200

    update

--- webapp/map.js
+++ webapp/map.js
@@ -6,53 +6,30 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-// Συντεταγμένες για τον Λευκό Πύργο και τη Ροτόντα
-var rotunda = L.marker([40.633233, 22.953213]).addTo(map)
-    .bindPopup("<b>Ροτοντα</b>");
 
-var whiteTower = L.marker([40.626238, 22.949157]).addTo(map)
-    .bindPopup("<b>Λευκός Πύργος</b>");
+var places = [
+    {
+        "title": "Λευκός Πύργος",
+        "description": "Ο Λευκός Πύργος είναι το πιο διάσημο μνημείο της Θεσσαλονίκης και σημείο αναφοράς της πόλης.",
+        "lat": 40.6407,
+        "lon": 22.9490
+    },
+    {
+        "title": "Ροτόντα",
+        "description": "Η Ροτόντα είναι ένας από τους αρχαιότερους και πιο εμβληματικούς χώρους της Θεσσαλονίκης.",
+        "lat": 40.6403,
+        "lon": 22.9446
+    },
+    {
+        "title": "Αγ. Δημήτριος",
+        "description": "Ο ναός του Αγίου Δημητρίου είναι ο μεγαλύτερος και πιο σημαντικός ναός της Θεσσαλονίκης.",
+        "lat": 40.6401,
+        "lon": 22.9444
+    }
+];
 
-var rotunda = L.marker([40.624082, 22.950343]).addTo(map)
-    .bindPopup("<b>ΑΓΑΛΜΑ ΜΕΓΑΛΟΥ ΑΛΕΞΑΝΔΡΟΥ</b>");
-var rotunda = L.marker([40.638244, 22.959717]).addTo(map)
-    .bindPopup("<b>ΚΗΠΟΙ ΤΟΥ ΠΑΣΑ</b>");
-var rotunda = L.marker([40.632152, 22.951896]).addTo(map)
-    .bindPopup("<b>ΑΨΙΔΑ ΤΟΥ ΓΑΛΕΡΙΟΥ</b>");
-
- // Δημιουργία χάρτη με OpenStreetMap μέσω του Leaflet.js
-var map = L.map('map').setView([40.6401, 22.9444], 13); // Θεσσαλονίκη, Ελλάδα
-
-// Χρησιμοποιούμε τον tile layer του OpenStreetMap
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-// Συντεταγμένες για τον Λευκό Πύργο και τη Ροτόντα
-var whiteTower = L.marker([40.6407, 22.9490]).addTo(map)
-    .bindPopup("<b>Λευκός Πύργος</b>")
-    .openPopup();
-
-var rotunda = L.marker([40.6403, 22.9446]).addTo(map)
-    .bindPopup("<b>Ροτόντα</b>");
-
-// Όταν κάνετε κλικ στις μαρκάδες, ανακατευθύνει στο gallery.html με την περιγραφή ως παράμετρο
-whiteTower.on('click', function () {
-    window.location.href = 'gallery.html?location=img-sights3'; // Ανακατεύθυνση με παράμετρο
+// Προσθήκη σημείων στον χάρτη από το JSON
+places.forEach(function(place) {
+    L.marker([place.lat, place.lon]).addTo(map)
+        .bindPopup("<b>" + place.title + "</b><br>" + place.description);
 });
-
-rotunda.on('click', function () {
-    window.location.href = 'gallery.html?location=Ροτόντα'; // Ανακατεύθυνση με παράμετρο
-});
-   
-
-fetch('md.json')
-    .then(response => response.json())
-    .then(data => {
-        data.forEach(location => {
-            console.log(`Τίτλος: ${location.title}`);
-            console.log(`Περιγραφή: ${location.description}`);
-            console.log(`Συντεταγμένες: ${location.lat}, ${location.lon}`);
-        });
-    })
-    .catch(error => console.log('Σφάλμα φόρτωσης:', error));
