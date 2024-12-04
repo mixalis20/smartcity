3fce48535a79d656aa3ad3a9a2c343def8542f14
mixalis20 <mtsatsakoulias@gmail.com>
2024-12-04 11:45:06 +0200

    dashb

--- webapp/js/dashboard1.js
+++ webapp/js/dashboard1.js
@@ -1,30 +1,37 @@
-// Συνάρτηση για να φορτώσουμε τα δεδομένα από το αρχείο JSON
+
 fetch('/webapp/json/dashboard.json')
     .then(response => response.json())
     .then(data => {
-        // Αναφορά των στοιχείων που θέλουμε να εμφανίσουμε
+        
         const dates = data.dates;
         const temperatures = data.temperatures;
+        const humidity = data.humidity;
+        const co2 = data.co2;
+        const air = data.air;
 
-        // Στοιχείο του dashboard που θα περιέχει τις κάρτες
+      
         const temperatureCardsContainer = document.querySelector('.temperature-cards');
 
-        // Για κάθε ημέρα και θερμοκρασία, δημιουργούμε μια κάρτα
+        
         dates.forEach((day, index) => {
             const temp = temperatures[index];
+            const hum = humidity[index];
+            const co2Value = co2[index];
+            const airValue = air[index];
 
-            // Δημιουργία της κάρτας
             const card = document.createElement('div');
             card.classList.add('temperature-card');
 
-            // Περιεχόμενο για την κάρτα
+      
             card.innerHTML = `
                 <div class="day">${day}</div>
                 <div class="temp">${temp}°C</div>
-                <div class="unit">Θερμοκρασία</div>
+                <div class="humidity">${hum}% Υγρασία</div>
+                <div class="co2">CO2: ${co2Value} ppm</div>
+                <div class="air">Αέρας: ${airValue} km/h</div>
             `;
 
-            // Προσθήκη της κάρτας στο container
+            
             temperatureCardsContainer.appendChild(card);
         });
     })
