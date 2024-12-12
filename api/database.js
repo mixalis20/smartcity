96e755140aaf3f5598aecfa17af04e4170b8414b
GEOVOUT06 <g.voutidis@gmail.com>
2024-12-12 12:22:55 +0200

    changed database.js

--- api/database.js
+++ api/database.js
@@ -8,27 +8,7 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
       // Cannot open database
       console.error(err.message)
       throw err
-    }else{
-        console.log('Connected to the SQLite database.')
-        db.run(`CREATE TABLE user (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name text, 
-            email text UNIQUE, 
-            password text, 
-            CONSTRAINT email_unique UNIQUE (email)
-            )`,
-        (err) => {
-            if (err) {
-                // Table already created
-            }
-            else{
-                // Table just created, creating some rows
-                var insert = 'INSERT INTO user (name, email, password) VALUES (?,?,?)'
-                db.run(insert, ["admin","admin@example.com",md5("admin123456")])
-                db.run(insert, ["user","user@example.com",md5("user123456")])
-            }
-        });  
-        
+    }else{  
         db.run(`CREATE TABLE sights (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             title TEXT,
